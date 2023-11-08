c732155b1af1b98827931ec1bd32731875534312
Luís Henrique <luishsilva09@gmail.com>
2023-11-08 11:44:28 -0300

    filtrando pelos niveis e turnos
    utilizando enuns para verificar cada nivel existe
    com codigo repitido

--- desafio.js
+++ desafio.js
@@ -41,63 +41,107 @@ const courses = {
       ],
     },
   },
+  italiano: {
+    avancado: {
+      manha: [
+        {
+          id: 13,
+        },
+        {
+          id: 23,
+        },
+      ],
+    },
+  },
 };
-let filter = ["ingles", "avancado", "manha"];
+let filter = ["avancado"];
 console.time("Tempo de execução ");
 
-function noFilter(courses) {
-  let res = [];
+const levels = {
+  iniciante: "iniciante",
+  avancado: "avancado",
+};
+const times = {
+  manha: "manha",
+  noite: "noite",
+  tarde: "tarde",
+  online: "online",
+};
+//filtro pelo curso ou gero um array
+function byCourse(courses, filtro) {
+  let response = [];
+
+  for (let element of filtro) {
+    if (courses[element]) {
+      response.push(courses[element]);
+      return response;
+    }
+  }
+  //caso nao tenha um filtro de curso retorna tudo
   for (let curso in courses) {
-    for (let nivel in courses[curso]) {
-      for (let turno in courses[curso][nivel]) {
-        courses[curso][nivel][turno].forEach((ele) => {
-          res.push(ele);
-        });
-      }
-    }
+    response.push(courses[curso]);
   }
-  return res;
+
+  return response;
 }
-function search(courses, filter) {
-  let res = [];
-  //caso o filtro estiver vazio
-  if (filter.length == 0) {
-    res = noFilter(courses);
-  }
-  //filtro de curso
-  filter.forEach((curso, index) => {
-    if (courses[curso]) {
-      for (let nivel in courses[curso]) {
-        if (nivel == filter[1]) {
-          for (let turno in courses[curso][nivel]) {
-            if (turno == filter[2]) {
-              courses[curso][nivel][turno].forEach((ele) => {
-                res.push(ele);
-              });
-            }
-            if (filter[2] == undefined) {
-              courses[curso][nivel][turno].forEach((ele) => {
-                res.push(ele);
-              });
-            }
-          }
+
+function byLevel(array, filtro) {
+  let response = [];
+
+  for (let i = 0; i <= filtro.length; i++) {
+    if (levels[filtro[i]]) {
+      for (let level of array) {
+        if (level[filtro[i]]) {
+          response.push(level[filtro[i]]);
         }
-        if (filter[1] == undefined) {
-          for (let turno in courses[curso][nivel]) {
-            courses[curso][nivel][turno].forEach((ele) => {
-              res.push(ele);
-            });
-          }
+      }
+      return response;
+    }
+    if (i == filtro.length) {
+      for (let level of array) {
+        for (let data in level) {
+          response.push(level[data]);
         }
       }
     }
-  });
-  return res;
+  }
+
+  return response;
 }
 
-// function byCourse(courses,filtro){
+function byTime(array, filtro) {
+  let response = [];
 
-// }
+  for (let i = 0; i <= filtro.length; i++) {
+    if (times[filtro[i]]) {
+      for (let time of array) {
+        if (time[filtro[i]]) {
+          response.push(time[filtro[i]]);
+          valid = false;
+        }
+      }
+      return response;
+    }
+    if (i == filtro.length) {
+      for (let time of array) {
+        for (let data in time) {
+          response.push(time[data]);
+        }
+      }
+    }
+  }
+
+  return response;
+}
+
+function search(courses, filtro) {
+  let response;
+  response = byCourse(courses, filtro);
+  response = byLevel(response, filter);
+  response = byTime(response, filter);
+
+  return response;
+}
 
 console.log(search(courses, filter));
 console.timeEnd("Tempo de execução ");
